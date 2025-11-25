c7aa36c27408db29e5a52dfdf025dd550adb5cee
Insha Mahfooz <insha8471@gmail.com>
2025-11-25 20:56:35 +0530

    a

--- src/utils/apiEndpoints.js
+++ src/utils/apiEndpoints.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://monerymanager-api-1.onrender.com/api/v1.0";
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://monerymanager-api-1.onrender.com/api/v1.0";
+// export const BASE_URL = "http://localhost:8080/api/v1.0";
 
 // cloudinary url
 const CLOUDINARY_CLOUD_NAME = "dsnxwhii2";
